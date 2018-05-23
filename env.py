f61f07670a4d02ed5c89389f9f4c1dc58d64fdcb
Mandali Vamshi Krishna <vamshi835@gmail.com>
2018-05-23 16:24:15 +0530

    Add files via upload

---
+++ env.py
@@ -0,0 +1,13 @@
+# Add Environment Variables
+
+DEBUG="False"
+SECRET_KEY='5(15ds+i2+%ik6z&!yer+ga9m="e%jcqiz_5wszg)r-z!2--b2d'
+DB_NAME="prem"
+DB_USER="postgres"
+DB_PASS="Vamshi835"
+DB_SERVICE="localhost"
+DB_PORT="5432"
+RKV_MAIL_GUN_KEY="replace_with_mailgun_key"
+NUZVID_MAIL_GUN_DOMAIN="rguktrkv.ac.in"
+NUZVID_MAIL_GUN_DOMAIN="rguktn.ac.in"
+NUZVID_MAIL_GUN_KEY="replace_with_mailgun_key"
