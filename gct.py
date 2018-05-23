1ec3883fcb1fd1f5436ae6317a7ec63470a9c8e7
Vamshi <vamshi835@gmail.com>
2018-05-23 09:51:51 +0530

    Latest

--- gct.py
+++ gct.py
@@ -633,6 +633,8 @@ def getAnswer(qid,path):
                         if op[0] == "=":
                             return op[1:len(op)]
 
+# ____________________________________________________________ Important Mehod ________________________________________________________________________
+
 def admin_login_required(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
@@ -659,6 +661,8 @@ def login_required(func):
             return render_template('unauthorized.html')
         return func(*args, **kwargs)
     return decorated_function
+
+# _____________________________________________________________________________________________________________________________________________________________
 
 def list_files(startpath):
     for root, dirs, files in os.walk(startpath):
